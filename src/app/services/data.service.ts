ec3b3e488277b7b2878c916f5a76190bcb6da94e
Yana Knysh <Yana.Knysh@dotdigital.com>
2023-01-03 17:55:11 +0300

    initial work for resizable component

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,13 @@
+﻿export class DataService{
+
+    private data: number[] = [ 150, 100];
+
+    getInitialSize(): number[] {
+
+        return this.data;
+    }
+    // addData(name: string){
+    //
+    //     this.data.push(name);
+    // }
+}
---
+++ src/app/svg/svg.component.ts
@@ -0,0 +1,32 @@
+﻿import {Component, OnInit, Renderer2} from '@angular/core';
+import {DataService} from "../services/data.service";
+
+@Component({
+    selector: 'app-svg',
+    templateUrl: './svg.component.svg',
+    styleUrls: ['./svg.component.css'],
+    providers: [DataService]
+})
+export class SvgComponent implements OnInit {
+    fillColor = 'rgb(255, 0, 0)';
+    
+    constructor(private datasevice: DataService, private renderer: Renderer2) {}
+
+    public changeColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        this.fillColor = `rgb(${r}, ${g}, ${b})`;
+    }
+    
+    getInitialSize(): number[]{
+        return this.datasevice.getInitialSize();
+    }
+
+    ngOnInit(): void {
+        // let initialSize = this.getInitialSize();
+        // var rect = document.getElementById('rectangle');
+        // this.renderer.setStyle(rect, 'height', initialSize[0]);
+        // this.renderer.setStyle(rect, 'width', initialSize[1]);
+    }
+}
